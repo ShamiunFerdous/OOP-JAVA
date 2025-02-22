cb4a0aaf46cec724e6f88128da1fe99864858ba7
S M Shamiun Ferdous <shamiunferdous1234@gmail.com>
2025-02-23 00:03:51 +0600

    Add files via upload

---
+++ src/com/Exception/UncheckedExceptionExample.java
@@ -0,0 +1,8 @@
+package com.Exception;
+
+public class UncheckedExceptionExample {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3};
+        System.out.println(arr[5]); // ArrayIndexOutOfBoundsException
+    }
+}
